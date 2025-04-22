da646ade5208a2647ebf4b4ff34e08df27b559a2
Angel lopez <zastuto5@gmail.com>
2025-04-22 15:04:23 -0500

    Killercs1

---
+++ src/Titel.jsx
@@ -0,0 +1,11 @@
+function Titel() {
+    const nombre = "Alumnos del V semestre - Ingenieria de software"
+    if (nombre) {
+        return <h1>Hola {nombre}</h1>
+
+    }
+    return <h1>Hola como estas</h1>
+
+}
+
+export default Titel;
